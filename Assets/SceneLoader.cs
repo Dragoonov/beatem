56d72b955c6319c24213dc685195f86f8f0aae68
Dragoonov <dragovonnova@gmail.com>
2019-10-27 16:59:57 +0100

    Added basic menu

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Button button;
+    public string levelName;
+    void Start()
+    {
+        button.GetComponent<Button>().onClick.AddListener(LoadLevel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
